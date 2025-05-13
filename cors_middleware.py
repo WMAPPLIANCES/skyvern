ac08c72c7d80e48a4f121b86cdb1c0b462f43dff
WMAPPLIANCES <148669330+WMAPPLIANCES@users.noreply.github.com>
2025-05-13 11:50:55 -0700

    Update cors_middleware.py

--- cors_middleware.py
+++ cors_middleware.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_cors(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://ui.skyvern.wmappliances.cloud"],
+        allow_origins=["https://ui.skyvern.wmappliances.cloud", "http://localhost:3000", "http://localhost:5173"], # Adicione as portas que você usa localmente
         allow_credentials=True,
         allow_methods=["*"],  # Todos os métodos
         allow_headers=["*"],  # Todos os cabeçalhos
