778ab43f03b04651aaaf3fee1dd28b68a5edc5e6
WMAPPLIANCES <148669330+WMAPPLIANCES@users.noreply.github.com>
2025-05-12 19:15:39 -0700

    Update entrypoint-skyvernui.sh

--- entrypoint-skyvernui.sh
+++ entrypoint-skyvernui.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-# Para desenvolvimento
-# npm run dev
-
-# Para produção
+# Compilar o frontend para produção
 npm run build
+
+# Servir o frontend compilado
 npm run preview -- --host 0.0.0.0 --port 80
